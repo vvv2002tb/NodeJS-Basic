b9592f9e82491ecf3d5c9a3cceb735d5ba941f98
vvv2002tb <92530486+vvv2002tb@users.noreply.github.com>
2022-08-27 16:01:34 +0700

    update

--- app.js
+++ app.js
@@ -4,12 +4,45 @@ console.log("Basic NodeJs");
 //     console.log("Server started after three seconds");
 // }, 3000);
 
-var time = 0;
+// var time = 0;
 
-var timer = setInterval(function() {
-    time += 2;
-    console.log("Server started after two seconds and auto restart", time);
-    if (time > 10) {
-        clearInterval(timer);
-    }
-}, 2000)
+// var timer = setInterval(function() {
+//     time += 2;
+//     console.log("Server started after two seconds and auto restart", time);
+//     if (time > 4) {
+//         clearInterval(timer);
+//     }
+// }, 2000)
+
+
+
+
+function callFunction () {
+    sayGoodBye();
+    sayHello();
+}
+
+
+
+
+
+// normal function gọi ở đâu cũng đc 
+function sayGoodBye() {
+    console.log("Goodbye!");
+}
+
+// sayGoodBye();
+
+
+// function Expression 
+
+// sayHello(); error
+
+const sayHello = function() {
+    console.log("Hello!");
+};
+
+// sayHello(); // chỉ được sử dụng sau khi khai báo 
+
+
+callFunction(); // nếu để callFunction trên đầu thì error, sau sayHello thì oke
---
+++ modules.js
@@ -0,0 +1,3 @@
+let dem = function (arrmang) {
+    
+}
