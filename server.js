f7cacbf1367d933c317b212a3b268d8ee583e088
Vinh Doan <clonevinh95@gmail.com>
2022-09-06 22:36:32 +0700

    server update

--- server.js
+++ server.js
@@ -11,52 +11,61 @@
 //     // ]
 //     // res.end(JSON.stringify(student));
 //     console.log('nguoi dung yeu cau link: ' + req.url);
-//     if(req.url === '/home' || req.url === '/'){ 
+//     if(req.url === '/home' || req.url === '/'){
 //         res.writeHead(200, {'Content-Type': 'text/html'});
 //         fs.createReadStream(__dirname + '/index.html', 'utf-8').pipe(res);
 
-//     } else if (req.url === '/login' || req.url === '/dangnhap'){ 
+//     } else if (req.url === '/login' || req.url === '/dangnhap'){
 //         res.writeHead(200, {'Content-Type': 'text/html'});
 //         fs.createReadStream(__dirname + '/login.html', 'utf-8').pipe(res);
 
-//     } else if (req.url === '/api/register'){ 
+//     } else if (req.url === '/api/register'){
 //         let register =  [{
 //             name: 'Vinh Doan Cristian',
 //             age: 20,
 //             role: 'admin',
-            
+
 //         }];
 //         res.writeHead(200, {'Content-Type': 'application/json'});
 //         res.end(JSON.stringify(register));
-       
-//     } 
-    
+
+//     }
+
 // });
 
 // server.listen(3000, '127.0.0.1');
 // console.log('Server running on port 3000');
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    // res.send('Hello World');
-    res.sendFile(__dirname + '/index.html', 'utf8');
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  // res.send('Hello World');
+  res.sendFile(__dirname + "/index.html", "utf8");
 });
 
-
-app.get('/login', (req, res) => {
-    // res.send('Hello World');
-    res.sendFile(__dirname + '/login.html', 'utf8');
+app.get("/login", (req, res) => {
+  // res.send('Hello World');
+  res.sendFile(__dirname + "/login.html", "utf8");
 });
 
-app.get('/contact/:userId/send/:content', (req, res) => {
-    res.send('This is contact id: ' + req.params.userId + ' send content ' + req.params.content  )
+// app.get('/contact/:userId/send/:content', (req, res) => {
+//     res.send('This is contact id: ' + req.params.userId + ' send content ' + req.params.content  )
+// });
+
+app.get("/register/:userId", (req, res) => {
+  const infor = {
+    name: "Sky",
+    age: 21,
+    email: "sky@gmail.com",
+    hobbies: ['book', 'soccer', 'learning']
+  };
+
+  res.render("register", { id: req.params.userId, information: infor });
 });
-
-
 
 app.listen(3000);
